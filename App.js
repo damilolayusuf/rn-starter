dbf29282f41d21af6442362ef1e476f1a5029d3d
damilolayusuf <yufstrategic@yahoo.co.uk>
2020-01-08 19:22:40 -0500

    Buttons and Screen Navigation

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+//import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
@@ -9,7 +11,7 @@ const navigator = createStackNavigator(
     List: ListScreen
   },
   {
-    initialRouteName: 'List',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
       title: 'App'
     }
